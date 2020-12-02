a63a5df4277961cafa78e2002f9822d0b8a692d4
YOSHINO Yoshihito <yy.y.ja.jp@gmail.com>
2020-12-03 00:07:46 +0900

    use hls; rtmp is now obsolete

--- rec_radiko.sh
+++ rec_radiko.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-playerurl=http://radiko.jp/apps/js/flash/myplayer-release.swf
 cookiefile=./cookie.txt
-playerfile=./player.swf
-keyfile=./authkey.png
 
 if [ $# -eq 1 ]; then
   channel=$1
-  output=./$1.flv
+  output=./$1.aac
 elif [ $# -eq 2 ]; then
   channel=$1
   output=$2
@@ -39,45 +36,41 @@ fi
 #
 # get player
 #
-if [ ! -f $playerfile ]; then
-  wget -q -O $playerfile $playerurl
+wget -q -N --no-if-modified-since http://radiko.jp/apps/js/playerCommon.js
 
-  if [ $? -ne 0 ]; then
-    echo "failed get player"
-    exit 1
-  fi
+if [ $? -ne 0 ]; then
+  echo "failed get player"
+  exit 1
 fi
 
 #
-# get keydata (need swftool)
+# get keydata
 #
-if [ ! -f $keyfile ]; then
-  swfextract -b 12 $playerfile -o $keyfile
+playerargs=`sed -n 's/.*new RadikoJSPlayer(\(.*\)/\1/p' ./playerCommon.js | sed 's/, /\n/g'`
+appid=`echo "$playerargs" | sed -n 2p | sed -e "s/^'//" -e "s/'$//"`
+authkey=`echo "$playerargs" | sed -n 3p | sed -e "s/^'//" -e "s/'$//"`
 
-  if [ ! -f $keyfile ]; then
-    echo "failed get keydata"
-    exit 1
-  fi
+if [ x = x"$authkey" ]; then
+  echo "failed get keydata"
+  exit 1
 fi
 
-if [ -f auth1_fms ]; then
-  rm -f auth1_fms
+if [ -f auth1 ]; then
+  rm -f auth1
 fi
 
 #
-# access auth1_fms
+# access auth1
 #
 wget -q \
-     --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_ts" \
-     --header="X-Radiko-App-Version: 4.0.0" \
-     --header="X-Radiko-User: test-stream" \
+     --header="X-Radiko-App: ${appid}" \
+     --header="X-Radiko-App-Version: 0.0.1" \
+     --header="X-Radiko-User: dummy_user" \
      --header="X-Radiko-Device: pc" \
-     --post-data='\r\n' \
      --no-check-certificate \
      --load-cookies $cookiefile \
      --save-headers \
-     https://radiko.jp/v2/api/auth1_fms
+     https://radiko.jp/v2/api/auth1
 
 if [ $? -ne 0 ]; then
   echo "failed auth1 process"
@@ -87,71 +80,94 @@ fi
 #
 # get partial key
 #
-authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms`
-offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1_fms`
-length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1_fms`
+authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1`
+offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1`
+length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1`
 
-partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
+partialkey=`echo -n "${authkey}" | dd bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
 
 echo "authtoken: ${authtoken} \noffset: ${offset} length: ${length} \npartialkey: $partialkey"
 
-rm -f auth1_fms
+rm -f auth1
 
-if [ -f auth2_fms ]; then
-  rm -f auth2_fms
+if [ -f auth2 ]; then
+  rm -f auth2
 fi
 
 #
-# access auth2_fms
+# access auth2
 #
 wget -q \
-     --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_ts" \
-     --header="X-Radiko-App-Version: 4.0.0" \
-     --header="X-Radiko-User: test-stream" \
-     --header="X-Radiko-Device: pc" \
      --header="X-Radiko-AuthToken: ${authtoken}" \
      --header="X-Radiko-PartialKey: ${partialkey}" \
-     --post-data='\r\n' \
+     --header="X-Radiko-User: dummy_user" \
+     --header="X-Radiko-Device: pc" \
      --load-cookies $cookiefile \
      --no-check-certificate \
-     https://radiko.jp/v2/api/auth2_fms
+     https://radiko.jp/v2/api/auth2
 
-if [ $? -ne 0 -o ! -f auth2_fms ]; then
+if [ $? -ne 0 -o ! -f auth2 ]; then
   echo "failed auth2 process"
   exit 1
 fi
 
 echo "authentication success"
 
-areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms`
+areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2`
 echo "areaid: $areaid"
 
-rm -f auth2_fms
+rm -f auth2
 
 #
-# get stream-url
+# get playlist-url
 #
 
 if [ -f ${channel}.xml ]; then
   rm -f ${channel}.xml
 fi
 
-wget -q "http://radiko.jp/v2/station/stream/${channel}.xml"
+wget -q "http://radiko.jp/v3/station/stream/${appid}/${channel}.xml"
 
-stream_url=`echo "cat /url/item[1]/text()" | xmllint --shell ${channel}.xml | tail -2 | head -1`
-url_parts=(`echo ${stream_url} | perl -pe 's!^(.*)://(.*?)/(.*)/(.*?)$/!$1://$2 $3 $4!'`)
+if [ x != x"$ft" ]; then
+  timefree=1
+else
+  timefree=0
+fi
+if [ -f $cookiefile ]; then
+  areafree=1
+else
+  areafree=0
+fi
+lsid=`date +%s999 -d '999999 seconds' | tr -d '\n' | md5sum | cut -d ' ' -f 1`
+playlist_urls=`echo "cat //url[@timefree='${timefree}'][@areafree='${areafree}']/playlist_create_url/text()" | xmllint -shell ${channel}.xml | grep ://`
+playlist_url=`echo "$playlist_urls" | head -1`
+#playlist_url=`echo "$playlist_urls" | grep -v tf-rpaa.smartstream.ne.jp | head -1`
+
+if [ $areafree = 0 ]; then
+  connectiontype='b'
+else
+  connectiontype='c'
+fi
+if [ $timefree = 0 ]; then
+  playlist_param="station_id=${channel}&l=15&lsid=${lsid}&type=${connectiontype}"
+else
+  playlist_param="station_id=${channel}&start_at=${ft}&ft=${ft}&end_at=${to}&to=${to}&l=15&lsid=${lsid}&type=${connectiontype}"
+fi
+
+if echo "$playlist_url" | grep -q -F '?'; then
+  playlist_url="${playlist_url}&${playlist_param}"
+else
+  playlist_url="${playlist_url}?${playlist_param}"
+fi
 
 rm -f ${channel}.xml
 
 #
-# rtmpdump
+# ffmpeg
 #
-rtmpdump -v \
-         -r ${url_parts[0]} \
-         --app ${url_parts[1]} \
-         --playpath ${url_parts[2]} \
-         -W $playerurl \
-         -C S:"" -C S:"" -C S:"" -C S:$authtoken \
-         --live \
-         --flv $output
+cp -i /dev/null "$output"
+ffmpeg -nostdin \
+       -headers "X-Radiko-AuthToken: ${authtoken}" \
+       -i "$playlist_url" \
+       -c copy -f adts pipe: > "$output"
+#       -c copy "$output"
